cb310465a4df5f2d6aac90f87d751fb29e00d111
ig <ivangavlik963@gmail.com>
2017-04-23 16:02:38 +0200

    test comit

--- 20170423/lab01/main.c
+++ 20170423/lab01/main.c
@@ -4,6 +4,24 @@
 /* 
 */
 
+unsigned sum(unsigned *niz, int n) {
+	unsigned s = 0, *q = niz;
+	
+	while(q != niz + n) {
+	
+		s = s + *(++q);
+	}
+	
+	
+	return s;
+}
+
 int main(void) {
+	unsigned a[10] = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+	
+	printf("%u \n", sum(a,10));
+	
 	return 0;
 }
+
+
